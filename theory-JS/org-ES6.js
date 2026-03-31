4c972c48bb7023ba60b64a5d8eb754532d81e2c8
arslan <244682724+justCallMeArslan@users.noreply.github.com>
2026-03-31 14:14:32 +0300

    Finish ES6 lesson, prepare for next lesson

---
+++ theory-JS/org-ES6.js
@@ -0,0 +1,7 @@
+// export default someVariable or "string" - can be used once ,
+
+// export {someVar, someVar}; - can be used multiple;
+
+// to link to HTML - entry point used , depending on what imports/exports from where
+
+// type="module" should be added to <script></script>
