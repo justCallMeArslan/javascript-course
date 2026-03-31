a165552eb38920528e1a76291df87f0280e4efe4
arslan <244682724+justCallMeArslan@users.noreply.github.com>
2026-03-31 14:32:54 +0300

    Finish npm

---
+++ theory-JS/org-npm.js
@@ -0,0 +1,19 @@
+// npm - package manager
+
+// using package.json fo manipulation of packages and dependies we know waht should be updated or downloaded
+
+// example of json:
+
+// {
+//     "name": "curriculum",
+//         "version": "1.0.0",
+//             "description": "[The Odin Project](https://www.theodinproject.com/) (TOP) is an open-source curriculum for learning full-stack web development. Our curriculum is divided into distinct courses, each covering the subject language in depth. Each course contains a listing of lessons interspersed with multiple projects. These projects give users the opportunity to practice what they are learning, thereby reinforcing and solidifying the theoretical knowledge learned in the lessons. Completed projects may then be included in the user's portfolio.",
+//                 "scripts": {
+//         "lint": "markdownlint-cli2",
+//             "fix": "markdownlint-cli2 --fix"
+//     },
+//     "license": "CC BY-NC-SA 4.0",
+//         "devDependencies": { // files which needed to be downloaded
+//         "markdownlint-cli2": "^0.12.1"
+//     }
+// }
