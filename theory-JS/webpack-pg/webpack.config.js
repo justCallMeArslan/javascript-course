bab57900f2ddb79cf9e1c5e581d5ae13b469c739
arslan <244682724+justCallMeArslan@users.noreply.github.com>
2026-04-02 12:01:43 +0300

    Change name of lesson file and update webpack folder up to CSS

--- theory-JS/webpack-pg/webpack.config.js
+++ theory-JS/webpack-pg/webpack.config.js
@@ -1,12 +1,18 @@
 import path from "node:path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
 
 export default {
     mode: "development",
     entry: "./src/index.js", // entry point
     output: { // output bundle object
         filename: "main.js", // name of bundle
-        path: path.resolve(import.meta.dirname, "dist"), 
+        path: path.resolve(import.meta.dirname, "dist"),
         clean: true, // each time Webpack runs it empties output directory, which 
         // helps to keep it clean, so it only contains the files produced by the latest
     },
+    plugins: [
+        new HtmlWebpackPlugin({ // making sure Webpack configuration has access to HtmlWebpackPlugin
+            template: "./src/template.html",
+        }) 
+    ]
 };
