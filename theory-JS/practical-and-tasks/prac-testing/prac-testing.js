948497c66c8ef61d4f0b6847804c6b6005050594
arslan <244682724+justCallMeArslan@users.noreply.github.com>
2026-05-22 15:38:38 +0300

    Complete until assignment 5

--- theory-JS/practical-and-tasks/prac-testing/prac-testing.js
+++ theory-JS/practical-and-tasks/prac-testing/prac-testing.js
@@ -1,3 +1,16 @@
 export function sum (a, b) {
     return a + b
 }
+
+
+export function floatingSum(x, y, z) {
+    return x + y - z;
+} 
+
+
+export const checkStr = "Timoha";
+
+export const checkStr1 = "Adam";
+
+export const cars = ["BMW", "Audi", "Merc", "Volvo", "VW", "Porsche"];
+
--- theory-JS/practical-and-tasks/prac-testing/prac-testing.test.js
+++ theory-JS/practical-and-tasks/prac-testing/prac-testing.test.js
@@ -1,5 +1,31 @@
-import { sum } from "./prac-testing.js";
+import { sum, floatingSum, checkStr, checkStr1, cars } from "./prac-testing.js";
+
+//numbers
 
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
 });
+
+
+// floating
+
+test("floating equation", () => {
+    expect(floatingSum(0.5, 0.5, 0.2)).toBeCloseTo(0.8); // max allowable difference is 0.005
+});
+
+// strings
+
+test("there is no X in Timoha", () => {
+    expect(checkStr).not.toMatch(/X/);
+})
+
+test("there is da in Adam", () => {
+    expect(checkStr1).toMatch(/da/);
+})
+
+// Arrays 
+
+test("is theree a Volvo in cars", () => {
+    expect(cars).toContain("Volvo");
+    expect(new Set(cars)).toContain("Volvo")
+})
