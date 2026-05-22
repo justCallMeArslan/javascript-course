92106afed8fffaaaf93d255eb2724f271f4cf436
arslan <244682724+justCallMeArslan@users.noreply.github.com>
2026-05-22 12:28:23 +0300

    Reach assignment 3

---
+++ theory-JS/test-basics.js
@@ -0,0 +1,3 @@
+// Test - driven development(TDD), is a new age evolutionary approach to development
+// which emphasizes test - first development. Here you write a test before you write 
+// just enough production code to fulfill that test and then go on to refactor the code.
