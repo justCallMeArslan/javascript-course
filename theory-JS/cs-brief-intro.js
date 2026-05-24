102704773a78a6c16d92f16a62908b10f3b6c368
arslan <244682724+justCallMeArslan@users.noreply.github.com>
2026-05-24 14:13:58 +0300

    Complete brief intro

---
+++ theory-JS/cs-brief-intro.js
@@ -0,0 +1,8 @@
+// Alghorithm is set of instructions for solvingproblems , step by step.
+
+
+// Pseudocode -  resemblance of computer language, written in any human language 
+// way to state an alghorithm.
+
+
+// Data structures - is the way to store data effeciently (performance and memory usage).
