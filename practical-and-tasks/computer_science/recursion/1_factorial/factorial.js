296f26490741cd0c0ac33017470f1afd29d13ae8
arslan <244682724+justCallMeArslan@users.noreply.github.com>
2026-05-27 17:40:38 +0300

    Complete exercise 1

--- practical-and-tasks/computer_science/recursion/1_factorial/factorial.js
+++ practical-and-tasks/computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,12 @@
-const factorial = function() {
-// same as JS infor example (start with JS info and update here)
+const factorial = function (n) {
+
+    if (!Number.isInteger(n) || typeof n !== "number" || n < 0) {
+        return undefined
+    }
+    if (n === 0) {
+        return 1;
+    }
+    return n === 1 ? 1 : factorial(n - 1) * n;
 };
 
 // Do not edit below this line
