1f330ca0282c83e4fb8335459893b1c91f09c365
arslan <244682724+justCallMeArslan@users.noreply.github.com>
2026-05-27 18:21:44 +0300

    Exercise 2 complete

--- practical-and-tasks/computer_science/recursion/2_contains/contains.js
+++ practical-and-tasks/computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,23 @@
-const contains = function() {
-  
+const contains = function (object, target) {
+    for (let key in object) { // search for a keys in object
+        let value = object[key]; // store value of each key:value 
+
+        if (value === target) { // checks for direct equaities
+            return true;
+        }
+
+        if (typeof value === "object" && value !== null) {
+            if (contains(value, target)) {
+                return true;
+            }
+        }
+
+        if ((Number.isNaN(value) && Number.isNaN(target)) || value === target) {
+            return true;
+        }
+    }
+    return false;
 };
-  
+
 // Do not edit below this line
 module.exports = contains;
