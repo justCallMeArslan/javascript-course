460cd86f74590dc3db510d7ae5568fa7a8870cef
arslan <244682724+justCallMeArslan@users.noreply.github.com>
2026-05-28 17:24:08 +0300

    Complete exercise 5

--- practical-and-tasks/computer_science/recursion/5_pascal/pascal.js
+++ practical-and-tasks/computer_science/recursion/5_pascal/pascal.js
@@ -1,6 +1,25 @@
-const pascal = function() {
-  
+const pascal = function (n) {
+
+    let first = [1];
+
+
+    for (let row = 1; row < n; row++) {
+
+        const newArr = [0, ...first, 0];
+        let next = [];
+
+        for (let j = 0; j < newArr.length - 1; j++) {
+            next.push(newArr[j] + newArr[j + 1]);
+        }
+
+        first = next;
+
+    }
+    return first;
 };
-  
+
+pascal(4);
+
+
 // Do not edit below this line
 module.exports = pascal;
