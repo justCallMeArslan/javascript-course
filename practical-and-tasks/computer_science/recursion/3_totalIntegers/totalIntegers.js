e56169ab8bd399e12bfa3b0e0503fb008e36b91e
arslan <244682724+justCallMeArslan@users.noreply.github.com>
2026-05-28 14:32:04 +0300

    Complete exercise 3

--- practical-and-tasks/computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ practical-and-tasks/computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,25 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function (data) {
+    if (data === null || typeof data !== "object") { // validating initial data 
+        return undefined;
+    }
+
+    function countCheck(value) {
+        let count = 0;
+        if (Number.isInteger(value)) {
+            return 1;
+        }
+        if (value === null || typeof value !== "object") {
+            return 0;
+        }
+        for (const key in value) {
+            count += countCheck(value[key])
+        }
+        return count;
+    }
+
+    return countCheck(data);
+
 };
-  
+
 // Do not edit below this line
 module.exports = totalIntegers;
