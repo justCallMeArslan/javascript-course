98c7f6a5a523f99220a6684cca08f24bfa31e069
arslan <244682724+justCallMeArslan@users.noreply.github.com>
2026-06-01 15:16:21 +0300

    End of TOP part

---
+++ theory-JS/cs-time-complexity.js
@@ -0,0 +1,33 @@
+// The way to measure code efficiency is to evaluate how many steps it takes 
+// to complete.
+
+// Assymptotic notations - used to describe the running time of an algorithm.
+// Main 3 are :
+
+
+// Big O notation - represents the upper bound of an algorithm, which is the 
+// worst case scenario for how the aglo perform.
+
+// Omega notation - represents the lower bound of an algorithm, which is the 
+// best case scenario
+
+// Theta notation - represents both upper bound and lower bound of an algorithm,
+// therefore analyses the average case complexity of an algorithm.
+
+
+// What is Big O?
+
+// It gives us a measurement for the time it takes for an algorithm to run as the 
+// input grows, so that we can directly compare the performance of two algorithms
+// and pick more efficient. 
+
+// the mostcommon Big O notations in the order of speed: 
+
+// - O(1) = Constant Complexity,
+// - O(log n) = Logarithmic Complexity,
+// - O(n) = Linear Complexity,
+// - O(n log n) = n * log n Complexity,
+// - O(n^2) = Quadratic Compleity,
+// - O(n^3) = Cubic Complexity,
+// - O(2^n) = Exponential Complexity,
+// - O(n!) = Factorial Complexity
