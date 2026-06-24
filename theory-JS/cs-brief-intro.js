7e0b279c10b1057ff8931ed9952c85bcea769d96
arslan <244682724+justCallMeArslan@users.noreply.github.com>
2026-06-24 12:01:32 +0300

    updated definiition of pseudocode

--- theory-JS/cs-brief-intro.js
+++ theory-JS/cs-brief-intro.js
@@ -2,7 +2,8 @@
 
 
 // Pseudocode -  resemblance of computer language, written in any human language 
-// way to state an alghorithm.
+// way to state an alghorithm. Better practice to keep it as close as possible to 
+// event or steps style.
 
 
 // Data structures - is the way to store data effeciently (performance and memory usage).
