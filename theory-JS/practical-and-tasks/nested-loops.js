5da760bdbd85f7593e448d2e6a697468d223442b
arslan <244682724+justCallMeArslan@users.noreply.github.com>
2026-06-26 20:54:46 +0300

    docs: provide comments

--- theory-JS/practical-and-tasks/nested-loops.js
+++ theory-JS/practical-and-tasks/nested-loops.js
@@ -27,9 +27,9 @@ for (let i = 1; i <= 5; i++) {
 // 5 4 3 2
 // 5 4 3 2 1
 
-for (let i = 1; i <= 5; i++) {
+for (let i = 1; i <= 5; i++) { // rows
     let line = "";
-    for (let j = 5; j >= 6 - i; j--) {
+    for (let j = 5; j >= 6 - i; j--) { // numbers, 6 - 1 help for pyramyde conture
         line += j + " "
 
     }
